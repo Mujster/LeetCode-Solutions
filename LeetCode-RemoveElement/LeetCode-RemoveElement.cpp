d296e0c53bb9b83304cb5fba7a2f9bd0a5e50b12
mujtaba ahmad <mmujtabach1234@gmail.com>
2024-02-04 02:31:38 +0500

    LeetCode solutions Update

--- LeetCode-RemoveElement/LeetCode-RemoveElement.cpp
+++ LeetCode-RemoveElement/LeetCode-RemoveElement.cpp
@@ -1,23 +1,19 @@
-//#include<iostream>
-//#include<vector>
-//using namespace std;
-//
-//class Solution {
-//public:
-//	int removeElement(vector<int>& nums, int val){
-//		vector<int> temp;
-//		int i,j;
-//		for (i = 0,j=0; i < nums.size(); i++) {
-//			if (nums.at(i) != val) {
-//				temp.push_back(nums.at(i));
-//				j++;
-//			}
-//		}
-//		nums = temp;
-//		return j++;
-//	}
-//};
-//
-//int main() {
-//	//Use gpt to generate main
-//}
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+	int removeElement(vector<int>& nums, int val){
+		vector<int> temp;
+		int i,j;
+		for (i = 0,j=0; i < nums.size(); i++) {
+			if (nums.at(i) != val) {
+				temp.push_back(nums.at(i));
+				j++;
+			}
+		}
+		nums = temp;
+		return j++;
+	}
+};
--- LeetCode-Sum/LeetCode-Sum.cpp
+++ LeetCode-Sum/LeetCode-Sum.cpp
@@ -1,25 +1,25 @@
-//#include<iostream>
-//#include<vector>
-//using namespace std;
-//
-//class Solution {
-//public:
-//    vector<int> twoSum(vector<int>& nums, int target) {
-//        vector<int> temp;
-//        for (int i = 0; i < nums.size(); i++) {
-//            for (int j = i + 1; j < nums.size(); j++) {
-//                if (nums.at(i) + nums.at(j) == target) {
-//                    temp.push_back(i);
-//                    temp.push_back(j);
-//                    break;
-//                }
-//            }
-//        }
-//        return temp;
-//    }
-//};
-//
-//int main() {
-//   //Use Gpt to generate Main
-//    return 0;
-//}
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+   vector<int> twoSum(vector<int>& nums, int target) {
+       vector<int> temp;
+       for (int i = 0; i < nums.size(); i++) {
+           for (int j = i + 1; j < nums.size(); j++) {
+               if (nums.at(i) + nums.at(j) == target) {
+                   temp.push_back(i);
+                   temp.push_back(j);
+                   break;
+               }
+           }
+       }
+       return temp;
+   }
+};
+
+int main() {
+  //Use Gpt to generate Main
+   return 0;
+}
