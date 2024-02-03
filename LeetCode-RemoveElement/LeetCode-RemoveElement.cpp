effcbebe6e2661b796b39feac4d9e506efbcc38a
mujtaba ahmad <mmujtabach1234@gmail.com>
2024-02-04 02:26:59 +0500

    LeetCode Solutions Update

---
+++ LeetCode-RemoveElement/LeetCode-RemoveElement.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//class Solution {
+//public:
+//	int removeElement(vector<int>& nums, int val){
+//		vector<int> temp;
+//		int i,j;
+//		for (i = 0,j=0; i < nums.size(); i++) {
+//			if (nums.at(i) != val) {
+//				temp.push_back(nums.at(i));
+//				j++;
+//			}
+//		}
+//		nums = temp;
+//		return j++;
+//	}
+//};
+//
+//int main() {
+//	//Use gpt to generate main
+//}
